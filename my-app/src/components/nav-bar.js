31fa161e3d9b99f98c19da140d3fa851b6c5c302
Erik Behnke <erik.behnke@gmail.com>
2019-03-19 15:42:08 -0500

    patch react files

---
+++ my-app/src/components/nav-bar.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import App from "../App";
+import {
+    Container,
+    Navbar
+} from 'reactstrap';
+
+class Navbar extends Component {
+    render() {
+        return (
+            <Container>
+                <p>test</p>
+            </Container>
+        );
+    }
+}
+
+export default Navbar;
