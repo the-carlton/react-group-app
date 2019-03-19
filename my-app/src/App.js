c66c673e4f8d3c78134c662b2fee42f19817f08c
Erik Behnke <erik.behnke@gmail.com>
2019-03-19 15:45:09 -0500

    added things

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
+import Navbar from 'components/nav-bar.js';
 
 class App extends Component {
   render() {
     return (
-        <h1>Hello World!</h1>
+        <Navbar/>
     );
   }
 }
