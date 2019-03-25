ef7a68db33c60745c57eafc2ca567ebffc526cf9
Josh Markasky <markasky.Josh@gmail.com>
2019-03-25 10:11:35 -0500

    added clock and form component

---
+++ my-app/src/components/Form.js
@@ -0,0 +1,62 @@
+//React Form Component
+import React, { Component } from 'react';
+import './form.css';
+import {
+    Container,
+    Row,
+    Col,
+    Form,
+    FormGroup,
+} from 'reactstrap';
+
+class Form1 extends Component{
+    constructor (props) {
+        super(props);
+        this.state = {
+            inputvalue: ''
+        };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleSubmit (event) {
+        console.log('Form value one: ' + this.state.inputvalue + ' and two: ' + this.state.inputvalue2);
+        event.preventDefault();
+    }
+
+    handleChange (event) {
+        let output = document.getElementById('output');
+        this.setState({
+            inputvalue: event.target.value,
+            output: output
+        })
+    }
+
+
+    componentDidMount() {
+
+    }
+
+    render(){
+        return(
+            <Container id="formCon">
+                <Row id={"row"}>
+                    <div id="form1" className="mx-auto">
+                        <form className="form-group" onSubmit={this.handleSubmit.bind(this)}>
+                            <label>Name</label><br/>
+                            <input className="form-control" type="text" value={this.state.inputvalue} onChange={this.handleChange.bind(this)}/><br/>
+                            <input className="form-control" type="submit" value="Submit"/>
+                        </form>
+                    </div>
+                </Row>
+                <Row>
+                    <div id="output" >{ this.state.inputvalue }</div>
+                </Row>
+            </Container>
+
+        )
+    }
+}
+
+export default Form1;
